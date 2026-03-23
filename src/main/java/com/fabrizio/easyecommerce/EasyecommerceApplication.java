e2aa5a4aa233fa864333530cbce0f5f6a2a8d915
fabrizio <fabrizioarmada3@gmail.com>
2026-03-23 12:04:58 -0300

    Initial commit

---
+++ src/main/java/com/fabrizio/easyecommerce/EasyecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.fabrizio.easyecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrizio/easyecommerce/EasyecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrizio.easyecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
